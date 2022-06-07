53a76640cf7d496b80b7c2d890d5edddec2bc44f
Igor <igorcbraz1@gmail.com>
2022-06-07 18:12:24 -0300

    Setting routes

--- src/routes.ts
+++ src/routes.ts
@@ -3,9 +3,13 @@ import { Router }  from "express";
 import { user } from './controller/userController'
 import { login } from "./controller/loginController";
 import { profession } from "./controller/professionController";
+import { patient } from "./controller/patientController";
+import { specialist } from "./controller/specialistController";
 
 export const routes = Router();
 
 routes.use('/user', user);
 routes.use('/login', login)
 routes.use('/profession', profession)
+routes.use('/patient', patient)
+routes.use('/specialist', specialist)
